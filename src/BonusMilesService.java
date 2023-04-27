3e362df5263ba71033694a850154fa428340f79e
AnnaZernina <110138099+AnnaZernina@users.noreply.github.com>
2023-04-27 15:37:01 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+
+    public int calculate(int cost) {
+        int price = 100; //стоимость билета
+        int miles = price / 20; // кол-во руб для одной б/мили
+
+        return cost;
+    }
+}
